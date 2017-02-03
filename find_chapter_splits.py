4688bed3755019306f822f45151a2e5513ebe70b
Nick King <penguin@frozendesert.net>
2017-02-03 07:34:50 -0800

    Move splitpoints to closest iframe before chapter

--- find_chapter_splits.py
+++ find_chapter_splits.py
@@ -32,12 +32,24 @@ def enumerate_chapters(metadata):
 	return chapters
 
 
+def get_next_iframe(f):
+	line = f.readline()
+	if len(line) == 0:
+		return None
+	return timedelta(milliseconds=float(line.strip()))
+
+
 def main(filename, output='output.mkv', end_episode_thresh=30, min_episode_length=1200, list_episodes=False,
-	 iframes=None):
+	 iframe_filename=None):
 	metadata = get_metadata(filename)
 	chapters = filter(lambda x: x[1].enabled, enumerate_chapters(metadata))
+	iframe_file = None
+	if iframe_filename:
+		iframe_file = open(iframe_filename, 'rb')
 
 	episodes = []
+	if iframe_file:
+		iframes = [get_next_iframe(iframe_file), get_next_iframe(iframe_file)]
 
 	start_num = None
 	start_chapter = None
@@ -45,8 +57,19 @@ def main(filename, output='output.mkv', end_episode_thresh=30, min_episode_lengt
 		if start_num is None:
 			start_num = num
 			start_chapter = ch
+			if iframe_file and num < len(chapters):
+				start_chapter.start = iframes[0]
+
+		while iframe_file and iframes[1] is not None and ch.end > iframes[1]:
+			iframes.pop(0)
+			iframes.append(get_next_iframe(iframe_file))
 		duration = ch.end - ch.start
 		if duration.total_seconds() < end_episode_thresh:
+			if iframe_file and num < len(chapters):
+				ch.end = iframes[0]
+				if iframes[1] is not None:
+					iframes.pop(0)
+					iframes.append(get_next_iframe(iframe_file))
 			e = Episode(start_num, num, start_chapter, ch)
 			if (ch.end - start_chapter.start).total_seconds() < min_episode_length:
 				e.discard = True
@@ -76,4 +99,4 @@ if __name__ == "__main__":
 	parser.add_argument('--output', action='store')
 	args = parser.parse_args()
 	
-	main(args.input, output=args.output, iframes=args.iframes, list_episodes=args.list)
+	main(args.input, output=args.output, iframe_filename=args.iframes, list_episodes=args.list)
