cc51604895d7ea2b283f1a646f06bf5620475f31
Nick King <penguin@frozendesert.net>
2017-02-04 09:48:03 -0800

    Move IFrame handling to class

--- find_chapter_splits.py
+++ find_chapter_splits.py
@@ -16,6 +16,28 @@ class Episode(object):
 		self.discard = discard
 
 
+class IFrameSeeker(object):
+	def __init__(self, filename):
+		self._handle = open(filename, 'rb')
+		self.iframes = [self._get_next_iframe(), self._get_next_iframe()]
+
+	def next(self):
+		self.iframes.pop(0)
+		self.iframes.append(self._get_next_iframe())
+
+	def current(self):
+		return self.iframes[0]
+
+	def peek(self):
+		return self.iframes[1]
+
+	def _get_next_iframe(self):
+		line = self._handle.readline()
+		if len(line) == 0:
+			return None
+		return timedelta(milliseconds=float(line.strip()))
+
+
 def get_metadata(filename):
 	with open(filename, 'rb') as f:
 		return enzyme.MKV(f)
@@ -32,53 +54,56 @@ def enumerate_chapters(metadata):
 	return chapters
 
 
-def get_next_iframe(f):
-	line = f.readline()
-	if len(line) == 0:
-		return None
-	return timedelta(milliseconds=float(line.strip()))
-
-
 def main(filename, output='output.mkv', end_episode_thresh=30, min_episode_length=1200, list_episodes=False,
-	 iframe_filename=None):
+	 iframe_filename=None, new_episode_grace=2):
 	metadata = get_metadata(filename)
 	chapters = filter(lambda x: x[1].enabled, enumerate_chapters(metadata))
-	iframe_file = None
+	iframe_reader = None
 	if iframe_filename:
-		iframe_file = open(iframe_filename, 'rb')
+		iframe_reader = IFrameSeeker(iframe_filename)
 
 	episodes = []
-	if iframe_file:
-		iframes = [get_next_iframe(iframe_file), get_next_iframe(iframe_file)]
 
 	start_num = None
 	start_chapter = None
 	last_num = None
 	last_chapter = None
+	grace_remaining = new_episode_grace
 	for num, ch in chapters:
+		#print "Processing chapter length %s\t(offset @ %s)" % ((ch.end - ch.start).total_seconds(), ch.start)
 		last_num = num
 		last_chapter = ch
+		grace_remaining -= 1
 		if start_num is None:
 			start_num = num
 			start_chapter = ch
-			if iframe_file and num < len(chapters):
-				start_chapter.start = iframes[0]
+			if iframe_reader and num < len(chapters):
+				start_chapter.start = iframe_reader.current()
 
-		while iframe_file and iframes[1] is not None and ch.end > iframes[1]:
-			iframes.pop(0)
-			iframes.append(get_next_iframe(iframe_file))
+		# Advance I-Frame pointer to just before end of chapter
+		# Stops advance if at end of file
+		while iframe_reader and iframe_reader.peek() is not None and ch.end > iframe_reader.peek():
+			iframe_reader.next()
 		duration = ch.end - ch.start
-		if duration.total_seconds() < end_episode_thresh:
-			if iframe_file and num < len(chapters):
-				ch.end = iframes[0]
-				if iframes[1] is not None:
-					iframes.pop(0)
-					iframes.append(get_next_iframe(iframe_file))
+
+		# Tracks less than end_episode_thresh are considered the last chapter of the episode
+		if grace_remaining < 0 and duration.total_seconds() < end_episode_thresh:
+			if iframe_reader and num < len(chapters):
+				# Adjust end of chapter to current I-frame
+				ch.end = iframe_reader.current()
+
+				# Move to next I-Frame if one is available
+				if iframe_reader.peek() is not None:
+					iframe_reader.next()
 			e = Episode(start_num, num, start_chapter, ch)
+
+			# If episode is less than min_episode_length, discard
 			if (ch.end - start_chapter.start).total_seconds() < min_episode_length:
 				e.discard = True
 			episodes.append(e)
 			start_num = None
+			grace_remaining = new_episode_grace
+
 
 	if start_num is not None and (last_chapter.end - start_chapter.start).total_seconds() > min_episode_length:
 		episodes.append(Episode(start_num, last_num, start_chapter, last_chapter))
@@ -104,6 +129,8 @@ if __name__ == "__main__":
 	parser.add_argument('--iframes', action='store', default=None, help='filename of I-Frame list')
 	parser.add_argument('--list', action='store_true')
 	parser.add_argument('--output', action='store')
+	parser.add_argument('--end-episode-thresh', action='store', default=30, type=int)
 	args = parser.parse_args()
 	
-	main(args.input, output=args.output, iframe_filename=args.iframes, list_episodes=args.list)
+	main(args.input, output=args.output, iframe_filename=args.iframes, list_episodes=args.list,
+	     end_episode_thresh=args.end_episode_thresh)
--- split_video.sh
+++ split_video.sh
@@ -2,11 +2,13 @@
 
 [ -z "$1" ] && echo "usage: $0 input" && exit 1
 INPUT="$1"
+shift
 
 set -ex
 
 ./extract_iframes.sh "${INPUT}" "${INPUT}".frames
-./find_chapter_splits.py --iframes "${INPUT}".frames --output output.mkv "${INPUT}" | sh
+
+./find_chapter_splits.py --iframes "${INPUT}".frames --output output.mkv "${INPUT}" $@ | sh
 for i in $(ls output-*.mkv)
 do
   ./renumber_chapters.sh "$i"
