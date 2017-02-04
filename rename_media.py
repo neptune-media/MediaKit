62789f1365ff0c23b71ad595f41aceddc56fb89e
Nick King <penguin@frozendesert.net>
2017-02-03 19:35:10 -0800

    Add wrapper script for split_video and script to rename outputs

---
+++ rename_media.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+
+import argparse
+import os
+import re
+
+
+OUTPUT_FORMAT = "%s - s%02de%02d.mkv"
+
+
+def main(input_format, show_name, season, episode_start, work_dir, dry_run):
+	input_re = re.compile(input_format)
+	dir_files = os.listdir(work_dir)
+	potential_files = []
+	for f in dir_files:
+		if input_re.match(f) is not None:
+			potential_files.append(f)
+
+	output_files = []
+	episode_num = episode_start
+	for f in sorted(potential_files):
+		output_files.append((f, OUTPUT_FORMAT % (show_name, season, episode_num)))
+		episode_num += 1
+
+	for f in output_files:
+		print "%s -> %s" % (f[0], f[1])
+		os.rename(f[0], f[1])
+	
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('show_name', action='store')
+	parser.add_argument('work_dir', action='store')
+	parser.add_argument('--format', dest='input_format', action='store')
+	parser.add_argument('--season', action='store', type=int)
+	parser.add_argument('--episode_start', action='store', default=1, type=int)
+	parser.add_argument('--dry-run', action='store_true')
+	args = parser.parse_args()
+	
+
+	main(**vars(args))
---
+++ split_video.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo "usage: $0 input" && exit 1
+INPUT="$1"
+
+set -ex
+
+./extract_iframes.sh "${INPUT}" "${INPUT}".frames
+./find_chapter_splits.py --iframes "${INPUT}".frames --output output.mkv "${INPUT}" | sh
+for i in $(ls output-*.mkv)
+do
+  ./renumber_chapters.sh "$i"
+done
+
